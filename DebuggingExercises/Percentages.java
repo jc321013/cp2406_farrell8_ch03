d92021b7c1265233e9394038e49add5665f3b9e9
jc321013 <jared.marcolono@my.jcu.edu.au>
2016-08-27 14:22:43 +1000

    ggfhj

---
+++ DebuggingExercises/Percentages.java
@@ -0,0 +1,11 @@
+/**
+ * Created by jc321013 on 27/08/16.
+ */
+public class Percentages {
+    public static void main(String args[]){
+        final double display_number = 2.0;
+        double Number = 5;
+        double twiceNumber = Number * display_number;
+        System.out.println(" Current number: " + Number + " After double: " + twiceNumber);
+    }
+}
